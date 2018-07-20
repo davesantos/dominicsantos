b91760fb49d2ffa4f18a8d0ca7a1100f67351866
davesantos <info@davesantos.com>
2018-07-20 11:35:55 -0700

    Add bugged staticClick

--- js/all.js
+++ js/all.js
@@ -2,7 +2,35 @@ var galleryElems = document.querySelectorAll('.carousel');
 
 for ( var i = 0, total = galleryElems.length; i < total; i++ ) {
   var galleryElem = galleryElems[i];
-  new Flickity( galleryElem, {
+
+  var flkty = new Flickity( galleryElem, {
     prevNextButtons: false
   });
+
+  flkty.on( 'staticClick', function( event, pointer, cellElement, cellIndex ) {
+
+    if ( !cellElement ) {
+      return;
+    }
+      // go to next if current cell selected
+    if ( cellIndex == this.selectedIndex ) {
+      this.next( true );
+    } else {
+      this.on( 'select', cellIndex);
+      console.log(cellIndex)
+    }
+
+  });
 }
+
+// flkty.on( 'staticClick', function( event, pointer, cellElement, cellIndex ) {
+//   if ( !cellElement ) {
+//     return;
+//   }
+//     // go to next if current cell selected
+//   if ( cellIndex == flkty.selectedIndex ) {
+//     flkty.next( true );
+//   } else {
+//     $carousel.flickity( 'select', cellIndex );
+//   }
+// });
