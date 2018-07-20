9cb70d9657d88cedb50523d5cf4fbcd52db69e98
davesantos <info@davesantos.com>
2018-07-20 01:27:40 -0700

    Remove lazyload

--- js/all.js
+++ js/all.js
@@ -3,7 +3,6 @@ var galleryElems = document.querySelectorAll('.carousel');
 for ( var i = 0, total = galleryElems.length; i < total; i++ ) {
   var galleryElem = galleryElems[i];
   new Flickity( galleryElem, {
-    lazyLoad: 2,
     prevNextButtons: false
   });
 }
