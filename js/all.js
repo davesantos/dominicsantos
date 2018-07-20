dc5ec62b8e76c039f8935da7b537c700b3271ee3
davesantos <info@davesantos.com>
2018-07-20 01:31:31 -0700

    Revert settings

--- js/all.js
+++ js/all.js
@@ -3,7 +3,6 @@ var galleryElems = document.querySelectorAll('.carousel');
 for ( var i = 0, total = galleryElems.length; i < total; i++ ) {
   var galleryElem = galleryElems[i];
   new Flickity( galleryElem, {
-    prevNextButtons: false,
-    setGallerySize: false
+    prevNextButtons: false
   });
 }
