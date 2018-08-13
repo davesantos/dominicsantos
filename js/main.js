0c5c47753dd362f0c18e32eed3887a6ef728667d
davesantos <info@davesantos.com>
2018-08-12 21:56:27 -0700

    Add smooth-scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // import $ from 'jquery';
 var Flickity = require('flickity');
+var scroll = new SmoothScroll('a[href*="#"]');
 
 //------------------------------------------------------------
 //
@@ -85,43 +86,3 @@ function scrollEffect() {
 //  Scroll Smooth
 //
 //------------------------------------------------------------
-
-var targetOffset, currentPosition,
-    body = document.body,
-    button = document.getElementById('scrollButton'),
-    animateTime = 900;
-
-function getPageScroll() {
-  var yScroll;
-
-  if (window.pageYOffset) {
-    yScroll = window.pageYOffset;
-  } else if (document.documentElement && document.documentElement.scrollTop) {
-    yScroll = document.documentElement.scrollTop;
-  } else if (document.body) {
-    yScroll = document.body.scrollTop;
-  }
-  return yScroll;
-};
-
-if (button) {
-
-  button.addEventListener('click', function (event) {
-
-    targetOffset = document.getElementById(event.target.hash.substr(1)).offsetTop;
-    currentPosition = getPageScroll();
-
-    body.classList.add('in-transition');
-    body.style.WebkitTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
-    body.style.MozTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
-    body.style.transform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
-
-    window.setTimeout(function () {
-      body.classList.remove('in-transition');
-      body.style.cssText = "";
-      window.scrollTo(0, targetOffset);
-    }, animateTime);
-
-    event.preventDefault();
-  }, false);
-};
