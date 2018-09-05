9d4c3511f3e7618a0f635819eaa4ad2ee9a6c6b6
davesantos <info@davesantos.com>
2018-09-05 12:04:42 -0700

    Update variables

--- _webpack/main.js
+++ _webpack/main.js
@@ -1,6 +1,6 @@
 // import $ from 'jquery';
-var Flickity = require('flickity');
-var scroll = new SmoothScroll('a[href*="#"]');
+const Flickity = require('flickity');
+const scroll = new SmoothScroll('a[href*="#"]');
 
 //------------------------------------------------------------
 //
@@ -15,12 +15,12 @@ WebFontConfig = {
 };
 
 (function() {
-  var wf = document.createElement('script');
+  let wf = document.createElement('script');
   wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
     '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
   wf.type = 'text/javascript';
   wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
+  let s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(wf, s);
 })();
 
@@ -30,12 +30,12 @@ WebFontConfig = {
 //
 //------------------------------------------------------------
 
-var galleryElems = document.querySelectorAll('.carousel');
+const galleryElems = document.querySelectorAll('.carousel');
 
-for ( var i = 0, total = galleryElems.length; i < total; i++ ) {
-  var galleryElem = galleryElems[i];
+for ( let i = 0, total = galleryElems.length; i < total; i++ ) {
+  let galleryElem = galleryElems[i];
 
-  var flkty = new Flickity( galleryElem, {
+  let flkty = new Flickity( galleryElem, {
     prevNextButtons: false,
     lazyLoad: 2,
     adaptiveHeight: true
@@ -61,15 +61,15 @@ for ( var i = 0, total = galleryElems.length; i < total; i++ ) {
 //
 //------------------------------------------------------------
 
-var lastFixPos = 0;
-var threshold = 150; //sensitivity on scrolling
-var theHead = document.querySelector(".header");
+const lastFixPos = 0;
+const threshold = 150; //sensitivity on scrolling
+const theHead = document.querySelector(".header");
 
 window.addEventListener("scroll", scrollEffect);
 
 function scrollEffect() {
-  var st = window.scrollY;
-  var diff = Math.abs(window.scrollY - lastFixPos);
+  let st = window.scrollY;
+  let diff = Math.abs(window.scrollY - lastFixPos);
   if (diff > threshold || st < 100) {
     if (st < lastFixPos) {
       if (theHead.classList)
@@ -86,7 +86,6 @@ function scrollEffect() {
   }
 }
 
-
 //------------------------------------------------------------
 //
 //  External Links
@@ -94,8 +93,8 @@ function scrollEffect() {
 //------------------------------------------------------------
 
 function externalLinks() {
-  for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {
-    var b = c[a];
+  for(let c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {
+    let b = c[a];
     b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
   }
 };
