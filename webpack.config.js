ca98826a4166eb7da6a8f894af10e35ea4d90b6b
davesantos <info@davesantos.com>
2018-09-08 14:52:33 -0700

    Add imagemin plugin, update path name

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports =  {
   context: path.join(__dirname, '_webpack'),
@@ -7,13 +9,17 @@ module.exports =  {
   entry: './main.js',
   output: {
     path: path.join(__dirname, 'js', 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'
   },
   plugins: [
     new CleanWebpackPlugin(['dist'], { root: path.resolve(__dirname, 'js'), verbose: true }),
+    new CopyWebpackPlugin([{
+      from: path.resolve('_uploads'),
+      to: '../../uploads/',
+    }]),
+    new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i })
   ],
   module: {
-
     rules: [
 
     ]
